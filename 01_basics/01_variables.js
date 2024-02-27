a3e543ff1e175ca909ee09109f130c8c367ae981
Akashshishodia <145325721+Akashshishodia@users.noreply.github.com>
2024-02-27 05:57:07 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345;
+let accountName = "Akash Shishodia";
+var accountPassword = "#1123#";
+accountCity = "Pilkhuwa";
+
+console.log(accountId);
+
+/* accountId = 2222; :: not allowed
+
+console.log(accountId);   :: No change in const variable
+
+*/
+
+console.table([accountId, accountName, accountPassword, accountCity])
