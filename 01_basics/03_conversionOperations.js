6ac6975ec8874f46611453b545e995a7fdab342b
Akash shishodia <akash853300@gmail.com>
2024-04-14 18:46:31 +0530

    not complete

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,41 @@
 let value = "Akash";
 
-console.log(typeof value);       //string
-console.log(typeof (value));     //string (via method)
+//console.log(typeof value);       //string
+//console.log(typeof (value));     //string (via method)
+
+let valueInNumber = Number(value);
+//console.log(typeof valueInNumber);    //number
+//console.log(valueInNumber);           //NaN
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Deepak";
+
+let isLoggedInBoolean = Boolean(isLoggedIn);
+
+//console.log(isLoggedInBoolean);  //true
+
+// 1 => true; 0 => false
+// "" => false
+
+let someValue = 12;
+
+let someValueString = String(someValue);
+
+//console.log(someValueString);  //12
+
+//                **************************OPERATIONS****************************
+
+let rate = 3 ;
+
+let negRate = -rate;
+//console.log(negRate);  //-3
+
+console.log(2+2); //4
+console.log(2-2); //0
+console.log(2*3); //6
+console.log(2**3);  //8
+console.log(10/5);  //2
+console.log(10%5);  //0
