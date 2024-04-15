373b5399eb71e1d575721dc95ffc8861c72c04ee
Akash shishodia <akash853300@gmail.com>
2024-04-15 21:57:18 +0530

    Done conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -33,9 +33,36 @@ let rate = 3 ;
 let negRate = -rate;
 //console.log(negRate);  //-3
 
-console.log(2+2); //4
-console.log(2-2); //0
-console.log(2*3); //6
-console.log(2**3);  //8
-console.log(10/5);  //2
-console.log(10%5);  //0
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*3); //6
+// console.log(2**3);  //8
+// console.log(10/5);  //2
+// console.log(10%4);  //0
+
+let str1 = 4;
+let str2 = 5;
+
+//console.log(str1 + str2); //9
+
+// console.log("1" + 2);           //12
+// console.log(1 + "2");           //12
+// console.log("1" + 2 + 2);       //122
+// console.log(1 + 2 + "2");       //32
+
+// console.log(+true);                //1
+// console.log(+"");                  //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2      // NOt Good to assign value // less readblity
+
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter);       //101
+
+
+
+
+
