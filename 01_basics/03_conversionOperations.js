d9c4f90929694bd675387723052a969e3d4bebee
Akash shishodia <akash853300@gmail.com>
2024-04-22 17:40:56 +0530

    completed

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let value = "Akash";
 
-//console.log(typeof value);       //string
-//console.log(typeof (value));     //string (via method)
+console.log(typeof value);       //string
+console.log(typeof (value));     //string (via method)
 
 let valueInNumber = Number(value);
-//console.log(typeof valueInNumber);    //number
-//console.log(valueInNumber);           //NaN
+console.log(typeof valueInNumber);    //number
+console.log(valueInNumber);           //NaN
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,7 +15,7 @@ let isLoggedIn = "Deepak";
 
 let isLoggedInBoolean = Boolean(isLoggedIn);
 
-//console.log(isLoggedInBoolean);  //true
+console.log(isLoggedInBoolean);  //true
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,34 +24,34 @@ let someValue = 12;
 
 let someValueString = String(someValue);
 
-//console.log(someValueString);  //12
+console.log(someValueString);  //12
 
 //                **************************OPERATIONS****************************
 
 let rate = 3 ;
 
 let negRate = -rate;
-//console.log(negRate);  //-3
+console.log(negRate);  //-3
 
-// console.log(2+2); //4
-// console.log(2-2); //0
-// console.log(2*3); //6
-// console.log(2**3);  //8
-// console.log(10/5);  //2
-// console.log(10%4);  //0
+console.log(2+2); //4
+console.log(2-2); //0
+console.log(2*3); //6
+console.log(2**3);  //8
+console.log(10/5);  //2
+console.log(10%4);  //0
 
 let str1 = 4;
 let str2 = 5;
 
-//console.log(str1 + str2); //9
+console.log(str1 + str2); //9
 
-// console.log("1" + 2);           //12
-// console.log(1 + "2");           //12
-// console.log("1" + 2 + 2);       //122
-// console.log(1 + 2 + "2");       //32
+console.log("1" + 2);           //12
+console.log(1 + "2");           //12
+console.log("1" + 2 + 2);       //122
+console.log(1 + 2 + "2");       //32
 
-// console.log(+true);                //1
-// console.log(+"");                  //0
+console.log(+true);                //1
+console.log(+"");                  //0
 
 let num1, num2, num3
 
@@ -60,7 +60,18 @@ num1 = num2 = num3 = 2 + 2      // NOt Good to assign value // less readblity
 
 let gameCounter = 100
 ++gameCounter;
-//console.log(gameCounter);       //101
+console.log(gameCounter);       //101
+
+
+
+
+console.log(+true);    //1
+
+console.log(typeof +true);  //+ makes it a number(type)
+
+console.log(+"");  //0
+
+console.log(typeof +"");   // + makes it a number(type)
 
 
 
