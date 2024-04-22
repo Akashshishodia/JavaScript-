404d2046afed3b0af0545f51955512d00403ebb8
Akash shishodia <akash853300@gmail.com>
2024-04-22 17:39:10 +0530

    completed

---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+// console.log(+true);    //1
+
+// console.log(typeof +true);  //+ makes it a number(type)
+
+// console.log(+"");  //0
+
+// console.log(typeof +"");   // + makes it a number(type)
+
